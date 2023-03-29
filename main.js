932e4d4aaa0314e64cd3f605c32348a208ff9034
Federico Geretto <fede.geretto96@gmail.com>
2023-03-29 03:46:20 -0300

    aiwjdiawd

---
+++ main.js
@@ -0,0 +1,26 @@
+const palabras = ["Landing Page", "Ecommerce", "Responsive", "Personalizado"];
+const maquinaDeEscribir = document.getElementById("maquina-de-escribir");
+
+function escribirPalabra(palabra) {
+    let index = 0;
+    const intervalId = setInterval(() => {
+        maquinaDeEscribir.textContent = palabra.slice(0, index);
+        index++;
+    if(index > palabra.length){
+      clearInterval(intervalId);
+      setTimeout(() => {
+        maquinaDeEscribir.textContent = "";
+        escribirSiguientePalabra();
+      }, 1000);
+    }
+  }, 100);
+}
+
+let indicePalabrasActual = 0;
+function escribirSiguientePalabra() {
+  const indicePalabra = palabras[indicePalabrasActual];
+  indicePalabrasActual = (indicePalabrasActual + 1) % palabras.length;
+  escribirPalabra(indicePalabra);
+}
+
+escribirSiguientePalabra();
