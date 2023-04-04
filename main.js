b56f0dc7ac75264829d609f79bec01c2d5bce296
Federico Geretto <fede.geretto96@gmail.com>
2023-04-04 18:52:45 -0300

    casi final

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+// ABRIR Y CERRAR MENÚ
+
+const abrirMenu = document.querySelector("#abrir-menu");
+
+const cerrarMenu = document.querySelector("#cerrar-menu");
+
+const navBar = document.querySelector("#navbar");
+
+
+abrirMenu.addEventListener("click", () => {
+    navBar.classList.add("activo");
+});
+
+cerrarMenu.addEventListener("click", () => {
+    navBar.classList.add("cerrar");
+
+    setTimeout(() => {
+        navBar.classList.remove("activo");
+        navBar.classList.remove("cerrar");
+    }, 300)
+});
+
+document.addEventListener("click", (event) => {
+  // Verifica si el elemento que se hizo clic está dentro del menú
+  if (!navBar.contains(event.target) && !abrirMenu.contains(event.target)) {
+      navBar.classList.remove("activo");
+  }
+});
+
+const enlacesMenu = document.querySelectorAll("#navbar li a");
+console.log(enlacesMenu);
+const arrayEnlacesMenu = Array.from(enlacesMenu);
+const arrayEnlacesMenuCerrar = arrayEnlacesMenu.slice(0, 3)
+arrayEnlacesMenuCerrar.forEach((enlace) => {
+  enlace.addEventListener("click", () => {
+      navBar.classList.remove("activo");
+  });
+});
+
+
+// ESCRIBIR PALABRAS
+
 const palabras = ["Landing Page", "Ecommerce", "Responsive", "Personalizado"];
 const maquinaDeEscribir = document.getElementById("maquina-de-escribir");
 
